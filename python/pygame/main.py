d6c956d2decc93e69b0c923910a8053343e2f4e6
sakaizu <angra0@naver.com>
2020-08-15 18:46:37 +0900

    upload

--- python/pygame/main.py
+++ python/pygame/main.py
@@ -21,6 +21,9 @@ pc_size = pcimage.get_rect().size
 pc_width = pc_size[0]
 pc_height = pc_size[1]
 
+# enemy image
+enemyimage = pygame.image.load(os.path.join('python/pygame', 'asset/enemy.png'))
+
 # projectile image
 projectileimage = pygame.image.load(os.path.join('python/pygame', 'asset/projectile.png'))
 
@@ -28,12 +31,21 @@ projectileimage = pygame.image.load(os.path.join('python/pygame', 'asset/project
 pc_pos_x = screen_width / 2 - (pc_width/2)
 pc_pos_y = screen_height - pc_height
 
+
+# Global variable
+running = True
+FPS = 60.0
+DeltaTime = 1/FPS
+Clock = pygame.time.Clock()
+font_main = pygame.font.SysFont("comicsans", 50)
 projectileList = []
+enemyList = []
+charList = []
 
 
 class Char:
 
-    COOLDOWN = FPS/5
+    FireRate = 5
 
     def __init__(self, x, y, speed):
         self.x = x
@@ -47,17 +59,23 @@ class Char:
         screen.blit(self.charimg, (self.x, self.y))
 
     def cooldown(self):
-        self.cool_down_counter += 1
-        if self.cool_down_counter >= self.COOLDOWN:
-            self.cool_down_counter = self.COOLDOWN
+        self.cool_down_counter += DeltaTime * self.FireRate
+        if self.cool_down_counter >= 1.0:
+            self.cool_down_counter = 1.0
         else:
-            self.cool_down_counter += 1
+            self.cool_down_counter += DeltaTime
 
-
-    def fire(self, vel):
-        if self.cool_down_counter == self.COOLDOWN:
-            projectileList.append(Projectile(self.x + (pc_width / 2), self.y, vel, 0, -1))
+    def fire(self, vel, dir_x, dir_y):
+        if self.cool_down_counter >= 1.0:
+            projectileList.append(Projectile(self.x + (pc_width / 2), self.y, vel, dir_x, dir_y))
             self.cool_down_counter = 0
+
+    def multfire(self, vel, dir_x, dir_y):
+        if self.cool_down_counter >= 1.0:
+            projectileList.append(Projectile(self.x + (pc_width / 2), self.y, vel, dir_x, dir_y))
+
+    def setcooltime(self):
+        self.cool_down_counter = 0
 
     def update(self):
         self.draw()
@@ -72,13 +90,23 @@ class Player(Char):
 
 
 class Enemy(Char):
-    def __init__(self, x, y, speed):
+    VARI_MAP = {
+                "1": (enemyimage),
+                "2": (enemyimage),
+                "3": (enemyimage)
+                }
+    def __init__(self, x, y, speed, var):
         super().__init__(x, y, speed)
-        self.charimg = pcimage
+        self.charimg = self.VARI_MAP[var]
         self.mask = pygame.mask.from_surface(self.charimg)
     
     def move(self):
         self.y += self.speed
+
+    def checkBottomFloor(self):
+        if self.y > screen_height:
+            pass
+            
 
 
 class Projectile:
@@ -90,13 +118,14 @@ class Projectile:
         self.img = projectileimage
         self.vel = vel
         self.Maxboundcount = 0
+        self.mask = pygame.mask.from_surface(self.img)
 
     def draw(self):
         screen.blit(self.img, (self.x - (projectileimage.get_width() / 2), self.y))
 
     def move(self):
-        self.x += self.dir_x * self.vel
-        self.y += self.dir_y * self.vel
+        self.x += self.dir_x * self.vel * DeltaTime
+        self.y += self.dir_y * self.vel * DeltaTime
 
     def collision(self, obj):
         return collide(self, obj)
@@ -113,37 +142,40 @@ class Projectile:
 
 
 
-
-running = True
-FPS = 60
-Clock = pygame.time.Clock()
-font_main = pygame.font.SysFont("comicsans", 50)
-
 # create player char
 PlayerChar = Player(pc_pos_x, pc_pos_y, 10)
-EnemyChar = Enemy(screen_width/2, 50, 1)
+enemyList.append(Enemy(screen_width/2, 50, 1, "1"))
+
 
 
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2, (offset_x, offset_y)) != None
+    return obj1.mask.overlap(obj2.mask, (int(offset_x), int(offset_y))) != None
 
 
 def updateDraw(): #Draw screen every tick
     screen.blit(bgimage, (0, 0))
     PlayerChar.update()
-    EnemyChar.draw()
-    EnemyChar.move()
+
+    for enemy in enemyList:
+        enemy.move()
+        enemy.update()
+    
+    for i in projectileList:
+        i.update()
+        for enemy in enemyList:
+            # print(i.collision(enemy))
+            if i.collision(enemy):
+                projectileList.remove(i)
+                print(enemy)
+
 
 # draw ui
     HP_label = font_main.render(f"HP: {PlayerChar.hp}", 1, (255,0,0))
     Pos_label = font_main.render(f"Pos: {PlayerChar.cool_down_counter}", 1, (0, 255, 0))
     screen.blit(HP_label, (10, 10))
     screen.blit(Pos_label, (10, 50))
-
-    for i in projectileList:
-        i.update()
 
 # update screen
     pygame.display.update()
@@ -172,8 +204,18 @@ while running:
         PlayerChar.y -= PlayerChar.speed
     if keys[pygame.K_DOWN] and PlayerChar.y + pc_height < screen_height:
         PlayerChar.y += PlayerChar.speed
+
+# character Fire
     if keys[pygame.K_SPACE]:
-        PlayerChar.fire(2)
+        PlayerChar.fire(200, 0, -1)
+
+        # for i in range(1, 11):
+        #     x = math.cos(float(i/10) * (math.pi * 2))
+        #     y = math.sin(float(i/10) * (math.pi * 2))
+        #     PlayerChar.multfire(200, x, y)
+        # PlayerChar.setcooltime()
+
+
 
 
 # game shut down
