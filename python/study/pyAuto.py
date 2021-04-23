19413327756c7c4f7debf7aa7b361adf3ebcf84d
sakaizu <angra0@naver.com>
2021-04-23 23:38:51 +0900

    update

--- python/study/pyAuto.py
+++ python/study/pyAuto.py
@@ -5,27 +5,7 @@ currentpos = ag.position()
 
 print(currentpos)
 
-ag.moveTo(1000, 1200)
-ag.typewrite(["winleft"])
-
-time.sleep(0)
-
-ag.typewrite("foobar")
-ag.typewrite(["enter"])
-
-time.sleep(0.25)
-
-#ag.hotkey('alt', 'f4')
-
-ag.typewrite(['alt', 'l', 's'])
-
-ag.typewrite("evangelion")
-
-time.sleep(0.3)
-
-ag.typewrite(["enter"])
-
-ag.hotkey('alt', 'f4')
+ag.moveRel(500, 0, 2)
 
 
 print("end")
--- python/study/test.py
+++ python/study/test.py
@@ -1,9 +1,29 @@
 from tkinter import *
 root = Tk()
-root.title("testwindows")
+root.title("MainWindows")
 root.geometry("640x480")
+
+def run1():
+    print(txt.get("1.0", END))
+    print(e.get())
+
+    txt.delete("1.0", END)
+    e.delete(0, END)
 
 btn1 = Button(root, text= "push")
 btn1.pack()
 
+btn3 = Button(root, fg = "red", bg = "yellow", text= 'Print', command=run1)
+btn3.pack()
+
+
+txt = Text(root, width=30, height=5)
+txt.pack()
+txt.insert(END, "insert num")
+
+e = Entry(root, width=30)
+e.pack()
+
+
 root.mainloop()
+
